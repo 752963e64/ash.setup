dad6cb6d4fb0a8f07dc5b446151e64448a33dcfb
DragonTalk <752963e64@tutanota.com>
2024-11-22 02:08:01 +0100

    Adds a bunch of function goodies... The scope is system wide... I need adds some check... so it don't blow in your hands...

--- etc/profile.d/90ashrc.sh
+++ etc/profile.d/90ashrc.sh
@@ -1,4 +1,50 @@
 # /etc/profile.d/**ashrc.sh
 
+ed25519() {
+  ssh-keygen -t ed25519 -C "$1"
+  ssh-add ~/.ssh/id_ed25519
+  cat ~/.ssh/id_ed25519.pub
+}
+
+# Open bar.
+chmod.fixup()
+{
+  for i in $(find . -type d); do chmod -v og+rx $i; done
+  for i in $(find . -type f); do chmod -v og+r $i; done
+}
+
+find.file.exec()
+{
+  for i in $(find . -type f -executable); do echo "$i"; done
+}
+
+urlencode() {
+  # urlencode <string>
+  local length=$((${#1}-1))
+  i=0; while [ $i -le $length ]; do
+    local c="${1:$i:1}"
+    case $c in
+      [a-zA-Z0-9.~_-]) printf '%s' "$c" ;;
+      *) printf '%%%02X' "'$c" ;;
+    esac
+    i=$((++i))
+  done
+}
+
+urldecode() {
+  # urldecode <string>
+  local url_encoded="${1//+/ }"
+  printf '%b' "${url_encoded//%/\\x}"
+}
+
+shellcheck() {
+  [[ -f "$1" ]] && curl -d script=$(urlencode "$(cat $1)") https://www.shellcheck.net/shellcheck.php | jq '.[] | .code,.message'
+}
+
+function host.io()
+{
+  [[ ${1} ]] && { curl "https://host.io/api/full/${1}?token=YOURTOKEN"; }
+}
+
 # find and load ~/.ashrc
 [ -f ~/.ashrc ] && . ~/.ashrc
