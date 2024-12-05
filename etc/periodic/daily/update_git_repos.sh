3cea900bb309525103292e4cbd51f11fb6e10301
DragonTalk <752963e64@tutanota.com>
2024-12-05 07:02:26 +0100

    toilet fflush.

--- etc/periodic/daily/update_git_repos.sh
+++ etc/periodic/daily/update_git_repos.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env sh
 
 # This script update a given directory with git repositories from any host...
-
-USERNAME="repos"
-
 # Because the script is run as root with crond being a system daemon.
 # We change access to "repos" user manualy. no password is required.
+
+USERNAME="repos"
 
 su - $USERNAME
 
