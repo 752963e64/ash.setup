3a2067e39bfa6770b67937edd6a9e64d477a9459
DragonTalk <752963e64@tutanota.com>
2025-11-05 09:31:43 +0100

    be verbose with mv,rm,mount,chown and chmod.

--- etc/profile.d/80aliases.sh
+++ etc/profile.d/80aliases.sh
@@ -2,9 +2,11 @@
 # system wide alias
 alias l='ls -alsh'
 alias mv='mv -v'
+alias rm='rm -v'
+alias chmod='chmod -v'
+alias chown='chown -v'
+alias mount='mount -v'
 alias ..='cd ..'
-alias todo='write todo'
-alias notice='write notice'
 alias ashrc='vi ~/.ashrc && . ~/.ashrc'
 alias aliases='vi ~/.aliases && . ~/.aliases'
 
