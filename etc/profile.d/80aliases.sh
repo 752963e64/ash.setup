906304679be2681d0fe32d412008f8dd188cc359
DragonTalk <752963e64@tutanota.com>
2025-11-22 22:42:33 +0100

    adds more aliases.   - setup root aliases.

--- etc/profile.d/80aliases.sh
+++ etc/profile.d/80aliases.sh
@@ -6,14 +6,18 @@ alias mv='mv -v'
 alias rm='rm -v'
 alias chmod='chmod -v'
 alias chown='chown -v'
-alias mount='mount -v'
-alias umount='umount -v'
-alias remount.ro='mount -o remount,ro'
-alias remount.rw='mount -o remount,rw'
-
 alias ..='cd ..'
 alias ashrc='vi ~/.ashrc && . ~/.ashrc'
 alias aliases='vi ~/.aliases && . ~/.aliases'
+
+if [ "$USER" = root ]; then
+  alias mount='mount -v'
+  alias umount='umount -v'
+  alias remount.ro='mount -o remount,ro'
+  alias remount.rw='mount -o remount,rw'
+  alias apk.update='apk update'
+  alias apk.upgrade='remount.rw && apk upgrade && remount.ro'
+fi
 
 # load user defined aliases.
 [ -f ~/.aliases ] && . ~/.aliases
