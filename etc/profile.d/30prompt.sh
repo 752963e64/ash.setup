b3ee6cb407b7a8ea1e10a23243b99c12b468e2e5
DragonTalk <752963e64@tutanota.com>
2025-11-23 08:39:50 +0100

    I was certainly drunk when I wrote that...

--- etc/profile.d/30prompt.sh
+++ etc/profile.d/30prompt.sh
@@ -2,16 +2,13 @@
 # Setup a red prompt for root and a green one for users.
 
 if [ "$USER" = root ]; then
-	_color=$'\e[0;91m'
-	_symbol='#'
-else
-	_color=$'\e[0;92m'
-	_symbol='$'
+  _color="1"
+  _symbol="#"
 fi
 
-PS1="\[\e]0;\u@\h: \w\a\]\[\e[1;94m\]┌──\[\e[1;94m\](\[$_color\]\u\["
-PS1="${PS1}\e[0;94m\]@\[\e[0;93m\]\h\[\e[1;94m\])\[\e[0;94m\]-\["
+PS1="\[\e]0;\u@\h: \w\a\]\[\e[1;94m\]┌──\[\e[1;94m\](\[\e[1;9${_color:-2}m\]\u\["
+PS1="${PS1}\e[1;94m\]@\[\e[1;96m\]\h\[\e[1;94m\])\[\e[1;94m\]-\["
 PS1="${PS1}\e[1;94m\][\[\e[0;92m\]\w\[\e[1;94m\]]\n\[\e[1;94m\]"
-PS1="${PS1}└─\[$_color\] $_symbol \[\e[0m\]"
+PS1="${PS1}└─\[\e[0;9${_color:-2}m\] ${_symbol:-$} \[\e[0m\]"
 unset _color _symbol
 
